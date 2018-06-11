a5390a8a4198f52a0698be0581de5590371d9dca
Zony <zony@myzony.com>
2018-06-11 17:49:31 +0800

    Add project files.

---
+++ Abp.Grpc.Client.Demo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Abp.Grpc.Client.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
